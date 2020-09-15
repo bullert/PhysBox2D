0dfd8f1d453d73206f9e1a164920ace8a597d6fa
bullert <b.bullert5@gmail.com>
2020-09-15 15:32:31 +0200

    first

---
+++ PhysBox2D/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PhysBox2D
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
